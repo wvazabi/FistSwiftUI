f5ea057018cfe11d8c33bda3ad6371e69fcd48c5
enes <eng.eneskaya@gmail.com>
2022-06-22 23:35:27 +0300

    Initial Commit

---
+++ FistSwiftUI/FistSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FistSwiftUIApp.swift
+//  FistSwiftUI
+//
+//  Created by Enes Kaya on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FistSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
